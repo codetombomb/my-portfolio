8ff7d33bdcc51148affbe3a1436662586ed16422
github username <github email address>
2020-07-07 15:17:13 -0500

    Made changes to the home page and pushing to GH to check functionality of netlify

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,8 @@ class Home extends Component {
             <div>
                 <h1>{this.props.title}</h1>
                 <h2>{this.props.homeInfo.subtitle}</h2>
+                <h2>{this.props.homeInfo.text}</h2>
+                <h2>{this.props.homeInfo.title}</h2>
             </div>
         )
     }
