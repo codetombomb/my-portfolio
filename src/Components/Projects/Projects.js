9f57a8069511bac4258dc54203a3c4398887b972
Tom Tobar <codetombomb@gmail.com>
2020-07-25 12:29:05 -0500

    Added project description

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,8 +8,8 @@ class Projects extends Component {
   }
   render() {
     const opts = {
-      height: "300",
-      width: "500",
+      height: "280",
+      width: "420",
       playerVars: {
         autoplay: 0,
       },
@@ -20,16 +20,26 @@ class Projects extends Component {
           {/* Covid Chaos */}
           <h3>Covid Chaos</h3>
           <YouTube id="covid-chaos" videoId={"gVurgKxb6l8"} opts={opts} />
+          <p>
+            Built using Javascript and Ruby on Rails, a dodging style game in which the player can accumulate points by collecting toilet paper rolls and hand sanitizer. The player must avoid Covid-19 viruses falling from above as well as other infected people falling from above.
+          </p>
         </div>
+
         <div className="demo-vid">
           {/* MotoGo */}
           <h3>MotoGo</h3>
           <YouTube id="motogo" videoId={"vbAduAp5svg"} opts={opts} />
+          <p>
+            A Motorcycle rental application built using React JS and Ruby on Rails that allows users to list their personal motorcycles for lease for a period of time and make money.
+        </p>
         </div>
+
         <div className="demo-vid">
           <h3>Jeopardy Lite</h3>
           <YouTube id="jeopardy" videoId={"YuLM-0J_7k8"} opts={opts} />
+          <p>A game made for the Command Line that gives users a single player version of Jeopardy that will prepare them for their appearance on the Jeopardy show</p>
         </div>
+
         <div className="demo-vid">
           {/* IceBreaker */}
           <h3>IceBreaker</h3>
@@ -38,6 +48,7 @@ class Projects extends Component {
             videoId={"H8kO7bLe3Cc"}
             opts={opts}
           />
+          <p>A Command Line Interface application that allows users to learn a numbers based fact that will allow them to start a conversation with someone new</p>
         </div>
       </div>
     );
