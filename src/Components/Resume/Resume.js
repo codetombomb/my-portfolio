cbd999ba9caab61a570b9a33ebf1a2a59043acb1
Tom Tobar <codetombomb@gmail.com>
2020-08-17 00:22:54 -0500

    Changed email address

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -7,7 +7,7 @@ import './Resume.css'
 const Resume = props => (
   <div className="resume">
     <h1>{props.title}</h1>
-    <p>Pflugerville, TX 78660 | 512-487-2569 | <a href={'mailto: codetombomb@gmail.com'}>codetombomb@gmail.com</a></p>
+    <p>Pflugerville, TX 78660 | 512-487-2569 | <a href={'mailto: devtomtobar@gmail.com'}>codetombomb@gmail.com</a></p>
     <p><a href={"https://github.com/codetombomb"} rel="noopener noreferrer" target="_blank">Github</a>| <a href={"https://medium.com/@codetombomb"} rel="noopener noreferrer" target="_blank">My Blog</a></p>
     <hr />
     <p>Junior Developer with Ruby on Rails and React.js based programming experience and a background in motorcycle mechanics. Excellent understanding of Full Stack Web Development and great customer service skills. With the high expectations he sets for himself and a goal oriented approach, Tom is able to work well under pressure to meet deadlines or demand. As a programmer, Tom is excited to continue learning and growing to fulfill his passion for web development.</p>
