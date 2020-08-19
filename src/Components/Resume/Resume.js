2bae3c1c14f3105a7f8b94e8d5d6867471f47bac
Tom Tobar <codetombomb@gmail.com>
2020-08-19 00:47:39 -0500

    Added MotoGo links and added styling to resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -6,15 +6,18 @@ import './Resume.css'
 
 const Resume = props => (
   <div className="resume">
-    <h1>{props.title}</h1>
-    <p>Pflugerville, TX 78660 | 512-487-2569 | <a href={'mailto: devtomtobar@gmail.com'}>codetombomb@gmail.com</a></p>
-    <p><a href={"https://github.com/codetombomb"} rel="noopener noreferrer" target="_blank">Github</a>| <a href={"https://medium.com/@codetombomb"} rel="noopener noreferrer" target="_blank">My Blog</a></p>
-    <hr />
-    <p>Junior Developer with Ruby on Rails and React.js based programming experience and a background in motorcycle mechanics. Excellent understanding of Full Stack Web Development and great customer service skills. With the high expectations I set for myself and my goal oriented approach, I am able to work well under pressure to meet deadlines or demand. As a programmer, I am excited to continue to learn and grow to fulfill my passion for web development.</p>
-    <h3>TECHNICAL SKILLS</h3>
-    <p style={{ fontWeight: "bold" }}>Ruby, Rails, SQLite3, Javscript, React, HTML, CSS</p>
-    <h3>TECHNICAL PROJECTS</h3>
-    <hr />
+    <div className="resume-child">
+      {/* <h1>{props.title}</h1> */}
+      <p>Pflugerville, TX 78660 | 512-487-2569 | <a href={'mailto: devtomtobar@gmail.com'}>codetombomb@gmail.com</a></p>
+      <p><a href={"https://github.com/codetombomb"} rel="noopener noreferrer" target="_blank">Github</a>| <a href={"https://medium.com/@codetombomb"} rel="noopener noreferrer" target="_blank">My Blog</a></p>
+      <hr />
+      <p>Junior Developer with Ruby on Rails and React.js based programming experience and a background in motorcycle mechanics. Excellent understanding of Full Stack Web Development and great customer service skills. With the high expectations I set for myself and my goal oriented approach, I am able to work well under pressure to meet deadlines or demand. As a programmer, I am excited to continue to learn and grow to fulfill my passion for web development.</p>
+      <h3>TECHNICAL SKILLS</h3>
+      <p style={{ fontWeight: "bold" }}>Ruby, Rails, SQLite3, Javscript, React, HTML, CSS</p>
+      <h3>TECHNICAL PROJECTS</h3>
+      <hr />
+      <h3 style={{ fontWeight: "bold" }}>MotoGo</h3><p><a href="https://github.com/codetombomb/MotoGoApp" rel="noopener noreferrer" target="_blank">Github</a> | <a href="https://youtu.be/vbAduAp5svg" rel="noopener noreferrer" target="_blank">Demo</a></p>
+    </div>
 
 
   </div>
