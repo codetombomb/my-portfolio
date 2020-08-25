b40cf661bb5fa9fff0a29ec62c1770137434e15b
Tom Tobar <codetombomb@gmail.com>
2020-08-25 15:24:25 -0500

    Resized icons--trying to debug red ring issue when deployed

--- src/Components/Social/SocialMedia.js
+++ src/Components/Social/SocialMedia.js
@@ -15,13 +15,13 @@ class SocialMedia extends Component {
         return (
             <div>
                 {/* Github */}
-                <SocialMediaIconsReact className="icon" icon="github" url="https://github.com/codetombomb" backgroundColor={"black"} iconColor={this.state.color} size={35} iconSize={5} />
+                <SocialMediaIconsReact className="icon" icon="github" url="https://github.com/codetombomb" backgroundColor={"black"} iconColor={this.state.color} size={35} iconSize={6} />
                 {/* Linkedin */}
-                <SocialMediaIconsReact className="icon" icon="linkedin" url="https://www.linkedin.com/in/tomtobar/" backgroundColor={"black"} iconColor={this.state.color} size={35} iconSize={5} />
+                <SocialMediaIconsReact className="icon" icon="linkedin" url="https://www.linkedin.com/in/tomtobar/" backgroundColor={"black"} iconColor={this.state.color} size={35} iconSize={6} />
                 {/* Twitter */}
-                <SocialMediaIconsReact className="icon" icon="twitter" url="https://twitter.com/tom_tobar" backgroundColor={"black"} iconColor={this.state.color} iconSize={5} />
+                <SocialMediaIconsReact className="icon" icon="twitter" url="https://twitter.com/tom_tobar" backgroundColor={"black"} iconColor={this.state.color} iconSize={6} />
                 {/* Instagram */}
-                <SocialMediaIconsReact className="icon" icon="instagram" url="https://www.instagram.com/tombomb_85/" backgroundColor={"black"} iconColor={this.state.color} size={35} iconSize={5} />
+                <SocialMediaIconsReact className="icon" icon="instagram" url="https://www.instagram.com/tombomb_85/" backgroundColor={"black"} iconColor={this.state.color} size={35} iconSize={6} />
             </div>
         )
     }
