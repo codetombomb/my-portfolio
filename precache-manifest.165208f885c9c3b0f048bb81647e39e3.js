9efdac37ae255da04acaa3ed7063e921722e8371
Tom Tobar <codetombomb@gmail.com>
2020-08-30 15:21:49 -0500

    Updates

---
+++ precache-manifest.165208f885c9c3b0f048bb81647e39e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136152673a489d0ef558528410b1522c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64c7450a4c4181211cb0",
+    "url": "/static/css/main.18a14591.chunk.css"
+  },
+  {
+    "revision": "7c2cd7873670ea775295",
+    "url": "/static/js/2.57410d54.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/static/js/2.57410d54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c7450a4c4181211cb0",
+    "url": "/static/js/main.8a33aa17.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "9fd2bc55995d8731e940fc8db77f0710",
+    "url": "/static/media/IMG_1196.9fd2bc55.jpeg"
+  },
+  {
+    "revision": "e8711e74ac6516c051e0a05bed437779",
+    "url": "/static/media/ttobar.e8711e74.pdf"
+  }
+]);
